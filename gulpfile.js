6aa9f150b31ff09530453763816000539c62145b
Dmitry <double0fork@gmail.com>
2019-01-10 07:13:02 +0300

    Done html mobile-lyaout by BEM

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat'); 
+var rename = require('gulp-rename');
+
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+	
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('watch', function () {
+   gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+});
