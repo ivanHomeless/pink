7f7c90f97b79d76993fc3f0984f10e57d1f946d4
Dmitry <double0fork@gmail.com>
2019-01-16 17:42:06 +0300

    Done scss-file bem structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var concat = require('gulp-concat'); 
+var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 
 sass.compiler = require('node-sass');
- 
+
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./sass/style.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(concat('style.css'))
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(gulp.dest('./css'))
-	
+
 	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./css'));
 });
- 
+
 gulp.task('watch', function () {
    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
 });
