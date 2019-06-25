0d9330c65cdcfb7cf28a5a8a345a431a83ca42b4
Dmitry <double0fork@gmail.com>
2019-06-25 18:20:28 +0300

    fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,71 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
+const server = require("browser-sync").create();
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const plumber = require("gulp-plumber");
+const minify = require("gulp-csso");
+const babel = require("gulp-babel");
+const uglify = require('gulp-uglify');
+const rename = require("gulp-rename");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const posthtml = require("gulp-posthtml");
+const include = require("posthtml-include");
+const del = require("del");
 
-sass.compiler = require('node-sass');
-
-gulp.task('sass', function () {
-  return gulp.src('./sass/style.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
-
-	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./css'));
+gulp.task("style", function() {
+    return gulp.src("source/sass/style.scss")
+        .pipe(plumber())
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(gulp.dest("build/css"))
+        .pipe(minify())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("build/css"))
+        .pipe(server.stream());
 });
 
-gulp.task('watch', function () {
-   gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+gulp.task("script", function() {
+    return gulp.src("source/js/**/*.js")
+        .pipe(gulp.dest("build/js"))
+        .pipe(babel({
+            presets: ["@babel/env"]
+        }))
+        .pipe(uglify())
+        .pipe(rename({suffix: ".min"}))
+        .pipe(gulp.dest("build/js")).on("change", server.reload);
 });
+
+gulp.task("html", function() {
+    return gulp.src("source/*.html")
+        .pipe(posthtml([
+            include()
+        ]))
+        .pipe(gulp.dest("build"));
+});
+
+gulp.task("copy", function() {
+    return gulp.src([
+        "source/images/**",
+        "source/pictures/**"
+        ], {
+            base: "source"
+        })
+        .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+    return del("build");
+});
+
+gulp.task("serve",  function() {
+    server.init({
+        server: "build/"
+    });
+
+    gulp.watch(["source/sass/**/*.{scss, sass}", "source/js/**/*.js"], gulp.parallel(["style", "script"]));
+    gulp.watch("source/*.html", gulp.parallel("html"));
+});
+
+gulp.task("build", gulp.series(["clean", "copy", "style", "script", "html"]));
